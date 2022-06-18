2764b1fec439e7a5399dbcb49be2e9b3a48cc80e
Arnish Shakil <arnish.shakil@gmail.com>
2022-06-18 11:24:51 -0400

    added more variety to the RNG

--- choreo.js
+++ choreo.js
@@ -1,14 +1,13 @@
-let randNum =  Math.floor(Math.random() * (3 - 1)) + 1;
+let randNum =  Math.floor(Math.random() * (9 - 1)) + 1;
 
-if (randNum === 1) {
+if (randNum === 1 || randNum === 4 || randNum === 7) {
     let compMove = rock;
 }
 
-if (randNum === 2) {
+if (randNum === 2 || randNum === 5 || randNum === 8) {
     let compMove = paper;
 }
 
-if (randNum === 3) {
+if (randNum%3 === 0) {
     let compMove = scissor;
 }
-
