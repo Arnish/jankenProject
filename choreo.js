eb4f22ed76702d0c1769b0cdd046a95dfdd323ac
Arnish Shakil <arnish.shakil@gmail.com>
2022-06-18 11:20:06 -0400

    added random moves for CPU

---
+++ choreo.js
@@ -0,0 +1,14 @@
+let randNum =  Math.floor(Math.random() * (3 - 1)) + 1;
+
+if (randNum === 1) {
+    let compMove = rock;
+}
+
+if (randNum === 2) {
+    let compMove = paper;
+}
+
+if (randNum === 3) {
+    let compMove = scissor;
+}
+
